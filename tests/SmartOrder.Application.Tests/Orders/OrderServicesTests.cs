1f2bcb6bd810a564c2efa733b39bf34fdc788c47
jasim <tjasim786@gmail.com>
2026-02-10 11:22:32 +0530

    test application added new scenarios

--- tests/SmartOrder.Application.Tests/Orders/OrderServicesTests.cs
+++ tests/SmartOrder.Application.Tests/Orders/OrderServicesTests.cs
@@ -1,9 +1,4 @@
 ﻿using SmartOrder.Domain.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Moq;
 using SmartOrder.Application.Services;
 using SmartOrder.Domain.Aggregates;
@@ -38,6 +33,50 @@ namespace SmartOrder.Application.Tests.Orders
             service.CreateOrderAsync(customerId));
         }
 
+        [Fact]
+        public async Task CreateOrder_Should_Create_Order_For_Eligible_Customer()
+        {
+            var customerId = Guid.NewGuid();
+            var customer = Customer.Create("Test", "test@mail.com");
+
+            _customerRepo.Setup(r => r.GetByIdAsync(customerId)).ReturnsAsync(customer);
+
+            _orderRepo.Setup(r => r.AddAsync(It.IsAny<Order>())).Returns(Task.CompletedTask);
+
+
+            _orderRepo.Setup(r => r.SaveChangesAsync()).Returns(Task.CompletedTask);
+
+            var service = CreateService();
+
+            var orderId = await service.CreateOrderAsync(customerId);
+
+            Assert.NotEqual(Guid.Empty, orderId);
+        }
+
+        [Fact]
+        public async Task AddItem_Should_Fail_When_Customer_Is_Not_Order_Owner()
+        {
+            var ownerId = Guid.NewGuid();
+            var attackerId = Guid.NewGuid();
+            var order = Order.Create(ownerId);
+
+            _orderRepo
+                .Setup(r => r.GetByIdAsync(order.Id))
+                .ReturnsAsync(order);
+
+            var service = CreateService();
+
+            await Assert.ThrowsAsync<InvalidOperationException>(() =>
+                service.AddItemToOrderAsync(
+                    order.Id,
+                    Guid.NewGuid(),
+                    1,
+                    attackerId
+
+                )
+            );
+        }
+
+
     }
 }
-~
